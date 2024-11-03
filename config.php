409c359a878aa21009adc34c5b8d150f33f24714
aleecook <taniempr@gmail.com>
2024-11-03 23:02:19 +0300

    config.php

--- config.php
+++ config.php
@@ -8,6 +8,6 @@ try {
     $db = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Ошибка подключения: " . $e->getMessage());
+    die("Ошибка подключения: " . $e->getMessage()); // Оповещение об ошибке
 }
 ?>
